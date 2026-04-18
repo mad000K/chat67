40db273a31962850d53fb940171570410fa4b06d
madOOOK <kravchenkomyronivka@gmail.com>
2026-04-18 19:32:51 +0300

    3

--- server.js
+++ server.js
@@ -25,4 +25,7 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
   console.log('a user connected. id - ' + socket.id);
+  socket.on('new_message', (message) => {
+    console.log(message);
+  });
 })
